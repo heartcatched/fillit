8e4a3882cda5346fac9dd2e0f0dd2935cfc9039c
Carole Briccant <cbriccan@at-d1.21-school.ru>
2020-01-17 15:02:32 +0300

    first version

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzoltan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 15:24:20 by wzoltan           #+#    #+#             */
+/*   Updated: 2019/11/04 15:32:33 by wzoltan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 100
+
+# include "fcntl.h"
+# include "sys/types.h"
+# include "sys/uio.h"
+# include "unistd.h"
+# include "stdlib.h"
+# include "libft.h"
+
+int		result(int rd, int fd, char **fub, char **line);
+int		get_line(char **fub, int fd, char **line);
+int		get_next_line(const int fd, char **line);
+
+#endif
